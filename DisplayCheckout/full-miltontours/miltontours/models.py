a1ed36d7cd5e228b892e8c9db9eca8bdf99fdbda
filipearena <filipe.gentil.arena@accenture.com>
2020-05-18 13:51:15 +1000

    added jinja logic to use base html and blocks of code more organized

--- DisplayCheckout/full-miltontours/miltontours/models.py
+++ DisplayCheckout/full-miltontours/miltontours/models.py
@@ -1,54 +1,17 @@
 from datetime import datetime
 
-class City:
-    def __init__(self, id, name, description, image):
+
+class Product:
+    def __init__(self, id, name, price, image):
         self.id = id
         self.name = name
-        self.description = description
-        self.image = image
-
-    def get_city_details(self):
-        return str(self)
-
-    def __repr__(self):
-        str = "Id: {}, Name: {}, Description: {}, Image: {} \n" 
-        str =str.format( self.id, self.name,self.description,self.image)
-        return str
-
-class Tour:
-    def __init__(self, id, name, description, image, price, city, date):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.image = image
         self.price = price
-        self.city = city
-        self.date = date
-    
-    def get_tour_details(self):
+        self.image = image
+
+    def get_product_details(self):
         return str(self)
 
     def __repr__(self):
-        str = "Id: {}, Name: {}, Description: {}, Image: {}, Price: {}, City: {}, Date: {}\n" 
-        str =str.format( self.id, self.name,self.description,self.image, self.price, self.city, self.date)
-        return str
-
-class Order:
-    def __init__(self, id, status, firstname, surname, email, phone, date, tours, total_cost):
-        self.id = id
-        self.status = status
-        self.firstname = firstname
-        self.surname = surname
-        self.email = email
-        self.phone = phone
-        self.date = date
-        self.tours = tours
-        self.total_cost = total_cost
-    
-    def get_tour_details(self):
-        return str(self)
-
-    def __repr__(self):
-        str = "id: {}, Status: {}, Firstname: {}, Surname: {}, Email: {}, Phone: {}, Date: {}, Tours: {}, Total Cost: {}\n" 
-        str =str.format( self.id, self.status,self.firstname,self.surname, self.email, self.phone, self.date, self.tours, self.total_cost)
+        str = "Id: {}, Name: {}, Price: {}, Image: {} \n" 
+        str =str.format( self.id, self.name,self.price,self.image)
         return str
--- DisplayCheckout/full-miltontours/miltontours/views.py
+++ DisplayCheckout/full-miltontours/miltontours/views.py
@@ -1,34 +1,43 @@
 from flask import Blueprint, render_template, url_for, request, session, flash
-from .models import City, Tour, Order
+from .models import Product
 from datetime import datetime
-from .forms import CheckoutForm
+# from .forms import CheckoutForm
 
-# This data will eventually be stored in a database
-sydney = City('1', 'Sydney', 'City in New South Wales with largest population', 'sydney.jpg')
-brisbane = City('2', 'Brisbane', 'City in Queensland with a good weather', 'brisbane.jpg')
-tour1 = Tour('1', 'Kangaroo point walk', 'Gentle stroll but be careful of cliffs. Hand feed the kangaroos', 't_hand.jpg', 99.00, brisbane, datetime(2020,7,23))
-tour2 = Tour('2', 'West End markets', 'Tour the boutique goods and food and ride the wheel', 't_ride.jpg', 20.00, brisbane, datetime(2019,10,30))
-tour3 = Tour('3', 'Whale spotting', 'Visit Straddy and see the whales migrating', 't_whale.jpg', 129.00, sydney, datetime(2019,10,30))
-cities = [brisbane,sydney]
-tours = [tour1,tour2,tour3]
-order1 = Order('1', False, '', '','', '', datetime.now(), [tour1,tour2], tour1.price+tour2.price) # simulating order not checked out
-order2 = Order('2', False, '', '','', '', datetime.now(), [tour3], tour1.price+tour3.price) # simulating order not checked out
-orders = [order1,order2]
+# # This data will eventually be stored in a database
+# sydney = City('1', 'Sydney', 'City in New South Wales with largest population', 'sydney.jpg')
+# brisbane = City('2', 'Brisbane', 'City in Queensland with a good weather', 'brisbane.jpg')
+# tour1 = Tour('1', 'Kangaroo point walk', 'Gentle stroll but be careful of cliffs. Hand feed the kangaroos', 't_hand.jpg', 99.00, brisbane, datetime(2020,7,23))
+# tour2 = Tour('2', 'West End markets', 'Tour the boutique goods and food and ride the wheel', 't_ride.jpg', 20.00, brisbane, datetime(2019,10,30))
+# tour3 = Tour('3', 'Whale spotting', 'Visit Straddy and see the whales migrating', 't_whale.jpg', 129.00, sydney, datetime(2019,10,30))
+# cities = [brisbane,sydney]
+# tours = [tour1,tour2,tour3]
+# order1 = Order('1', False, '', '','', '', datetime.now(), [tour1,tour2], tour1.price+tour2.price) # simulating order not checked out
+# order2 = Order('2', False, '', '','', '', datetime.now(), [tour3], tour1.price+tour3.price) # simulating order not checked out
+# orders = [order1,order2]
+
+
+product1 = Product('1', 'Star Wars - Ultimate Collector Millennium Falcon', 89.99, 'most_popular1.jpg')
+product2 = Product('2', 'Star Wards - Death Star', 79.99, 'most_popular2.jpg')
+product3 = Product('3', 'Ninjago - Masters of Spinjitzu', 49.99, 'most_popular3.jpg')
+mostpopular = [product1, product2,product3]
 
 bp = Blueprint('main', __name__)
 
-
 @bp.route('/')
 def index():
-    return render_template('home.html')
+    # for product in mostpopular:
+    #     mostpopular.append(product)
+    return render_template('home.html', products = mostpopular)
 
 @bp.route('/home')
 def home():
-    return render_template('home.html')
+    # for product in mostpopular:
+    #     mostpopular.append(product)
+    return render_template('home.html', products = mostpopular)
 
 @bp.route('/shop')
 def shop():
-    return render_template('shop.html')
+    return render_template('shop.html', products = mostpopular)
 
 @bp.route('/product')
 def product():
@@ -38,64 +47,64 @@ def product():
 # def index():
 #     return render_template('index.html', cities = cities)
 
-@bp.route('/tours/<int:cityid>/')
-def citytours(cityid):
-    citytours = []
-    # create list of tours for this city
-    for tour in tours:
-            if int(tour.city.id) == int(cityid): 
-                citytours.append(tour)
-    return render_template('citytours.html', tours = citytours)
+# @bp.route('/tours/<int:cityid>/')
+# def citytours(cityid):
+#     citytours = []
+#     # create list of tours for this city
+#     for tour in tours:
+#             if int(tour.city.id) == int(cityid): 
+#                 citytours.append(tour)
+#     return render_template('citytours.html', tours = citytours)
 
 
-@bp.route('/order/', methods=['POST','GET'])
-def order():
+# @bp.route('/order/', methods=['POST','GET'])
+# def order():
 
-    tour_id = request.args.get('tour_id')
-    # is this a new order?
-    if 'order_id'not in session:
-        session['order_id'] = 1 # arbitry, we could set either order 1 or order 2
+#     tour_id = request.args.get('tour_id')
+#     # is this a new order?
+#     if 'order_id'not in session:
+#         session['order_id'] = 1 # arbitry, we could set either order 1 or order 2
     
-    #retrieve correct order object
-    for x in orders:
-            if int(x.id) == int(session['order_id']): 
-                order = x
-    # are we adding an item? - will be implemented later with DB
-    if tour_id:
-        print('user requested to add tour id = {}'.format(tour_id))
+#     #retrieve correct order object
+#     for x in orders:
+#             if int(x.id) == int(session['order_id']): 
+#                 order = x
+#     # are we adding an item? - will be implemented later with DB
+#     if tour_id:
+#         print('user requested to add tour id = {}'.format(tour_id))
 
-    return render_template('order.html', order = order, totalprice = order.total_cost)
-
-
-@bp.route('/deleteorder/')
-def deleteorder():
-    if 'order_id' in session:
-        del session['order_id']
-    return render_template('index.html')
-
-@bp.route('/deleteorderitem/', methods=['POST'])
-def deleteorderitem():
-    print('User wants to delete tour with id={}'.format(request.form['id']))
-    return render_template('index.html')
+#     return render_template('order.html', order = order, totalprice = order.total_cost)
 
 
-@bp.route('/checkout/', methods=['POST','GET'])
-def checkout():
-    form = CheckoutForm() 
-    if 'order_id' in session:
+# @bp.route('/deleteorder/')
+# def deleteorder():
+#     if 'order_id' in session:
+#         del session['order_id']
+#     return render_template('index.html')
+
+# @bp.route('/deleteorderitem/', methods=['POST'])
+# def deleteorderitem():
+#     print('User wants to delete tour with id={}'.format(request.form['id']))
+#     return render_template('index.html')
+
+
+# @bp.route('/checkout/', methods=['POST','GET'])
+# def checkout():
+#     form = CheckoutForm() 
+#     if 'order_id' in session:
         
-        #retrieve correct order object
-        for x in orders:
-                if int(x.id) == int(session['order_id']): 
-                    order = x
+#         #retrieve correct order object
+#         for x in orders:
+#                 if int(x.id) == int(session['order_id']): 
+#                     order = x
        
-        if form.validate_on_submit():
-            order.status = True
-            order.firstname = form.firstname.data
-            order.surname = form.surname.data
-            order.email = form.email.data
-            order.phone = form.phone.data
-            print(order)
-            flash('Thank you for your information')
+#         if form.validate_on_submit():
+#             order.status = True
+#             order.firstname = form.firstname.data
+#             order.surname = form.surname.data
+#             order.email = form.email.data
+#             order.phone = form.phone.data
+#             print(order)
+#             flash('Thank you for your information')
 
-    return render_template('checkout.html', form = form)
+#     return render_template('checkout.html', form = form)
